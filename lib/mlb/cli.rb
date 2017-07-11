6aa1875205478d660fdd3cff6bc432ab943b12e7
derek pelt <derekpelt@gmail.com>
2017-07-11 02:09:20 -0400

    updated readme file

--- lib/mlb/cli.rb
+++ lib/mlb/cli.rb
@@ -1,49 +1,61 @@
 
 #our Cli Controller 
-
 class Mlb::CLI 
 
 	def call 
 		puts "Today's final scores."
 		list_games
 		menu 
-		goodbye
 	end 
 
 	def list_games
-		@games = Mlb::Game.today 
+		@games = Mlb::Game.scrape_games
 		@games.each.with_index(1) do |game, i|
-			puts "#{i}. #{game.winner}:#{game.score_w} #{game.loser}:#{game.score_l}"
+			puts "#{i}) #{game.winner}:#{game.score_w} #{game.loser}:#{game.score_l}"
 		end 
 
 	end 
 
 	def menu 
-		puts "\n find more information about a game\n \n"
-		input = ""
-		while input != "exit"
-			input = gets.strip.downcase
-			case input 
-			when "1"
-				print_more_info("1")
-			when "2"
-				print_more_info("2")
-			when "list"
-				list_games
-			else 
-				puts ("[[".red + "not sure what you want, please type list or exit".white + "]]".red)			
-			end 
-		end 
+		puts "\nSelect 1 2 3 or exit to leave\n"
+		puts "\n 1 - show more info on games"
+		puts "\n 2 - show standings"
+		puts "\n 3 - show biggest run differential"
+		input = gets.strip
+		if input.to_i == 1 
+			puts "enter a game number 1 - #{@games.count} to see more info"
+			input = gets.strip.to_i-1
+			print_more_info(input)
+			puts "\nWould you like to continue Y or N ? \n"
+			  input = gets.strip.downcase
+		    if input == "y"
+		    	menu 
+		    elsif input == 'n'
+		    	goodbye
+		    else
+		      puts "\nPlease enter Y or N only"
+		      menu 
+		    end 
+		elsif input.to_i == 2 
+			Mlb::Game.scrape_standings
+			menu 
+		elsif input.to_i == 3 
+			Mlb::Game.find_lopside_score
+			menu 
+		elsif input.downcase == "exit"
+			goodbye	
+		else 
+			menu 
+	    end	     	
 	end 
 
 	def print_more_info(input)
-		input = input.to_i-1
-		puts "winning pitcher:#{@games[input].pitcher_w} losing pitcher:#{@games[input].pitcher_l} #{@games[input].save}" 
+		puts "winning pitcher".green + ": #{@games[input].pitcher_w}".cyan + " losing pitcher".red + ": #{@games[input].pitcher_l}".cyan + " #{@games[input].save}" 
 	end 
 
-
 	def goodbye
-		puts "goodbye!".yellow 
+		puts "Thankyou! Have a great day!".yellow 
+		exit 
 	end 
 
 end 
--- lib/mlb/game.rb
+++ lib/mlb/game.rb
@@ -4,28 +4,19 @@ class Mlb::Game
 
 	attr_accessor :winner, :loser, :score_w, :score_l, :pitcher_w, :pitcher_l, :save
 
-	def self.today 
-		self.scrape_pitchers
-	end 
+	@doc = Nokogiri::HTML(open("http://www.baseball-reference.com/boxes/"))
 
-	def self.scrape_pitchers 
+	def self.scrape_games
 		games = [] 
-		
-		#goto mlb, find pitchers for a current day 
-		#extract the properties 
-		#instantiate all picthers from current day 
-		#doc = Nokogiri::HTML(open("https://www.mlb.com/scores/2017-07-06", ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
-		#binding.pry 
-		doc = Nokogiri::HTML(open("http://www.baseball-reference.com/boxes/"))
+
 		the_game = nil 
 		
-		
-		doc.css(".game_summary").each do |game|
+		@doc.css(".game_summary").each do |game|
 			the_game = self.new 
 		 	the_game.winner = game.css(".winner td[1]").text.green
 		 	the_game.loser = game.css(".loser td[1]").text.red
-		 	the_game.score_w = game.css(".winner td[2]").text.cyan
-			the_game.score_l = game.css(".loser td[2]").text.cyan
+		 	the_game.score_w = game.css(".winner td[2]").text
+			the_game.score_l = game.css(".loser td[2]").text
 			the_game.pitcher_w = game.css("table:nth-child(2) tr:nth-child(1) td:nth-child(2)").text.light_green
 			the_game.pitcher_l = game.css("table:nth-child(2) tr:nth-child(2) td:nth-child(2)").text.light_red
 			the_game.save = game.css("table:nth-child(2) tr:nth-child(3) td:nth-child(2)").text.yellow
@@ -33,11 +24,44 @@ class Mlb::Game
 				the_game.save = the_game.save + (" S").yellow 
 			end
 			games << the_game 
-
-
 		end 
 		games 
 	end 
+
+	def self.scrape_standings
+		puts "\n AL STANDINGS"
+		puts "\n TM  W  L  WIN%  GB"
+		@doc.css("#standings-upto-AL-overall tr").each do |row|
+		 	title = row.css("tr:nth-child(1)").text
+		 	team = row.css("th a").text
+		 	wins = row.css("td:nth-child(2)").text.green 
+		 	losses = row.css("td:nth-child(3)").text.red
+		 	win_percentage = row.css("td:nth-child(4)").text.cyan
+		 	games_back = row.css("td:nth-child(5)").text.yellow 
+	 	puts " #{team} #{wins} #{losses} #{win_percentage} #{games_back}"
+		end 
+
+		puts "\n NL STANDINGS"
+		puts "\n TM  W  L  WIN%  GB"
+		@doc.css("#standings-upto-NL-overall tr").each do |row|
+		 	team = row.css("th a").text
+		 	wins = row.css("td:nth-child(2)").text.green 
+		 	losses = row.css("td:nth-child(3)").text.red
+		 	win_percentage = row.css("td:nth-child(4)").text.cyan
+		 	games_back = row.css("td:nth-child(5)").text.yellow 
+		 	puts " #{team} #{wins} #{losses} #{win_percentage} #{games_back}"
+		 end 
+	end 
+
+
+	def self.find_lopside_score
+		scores  = [] 
+		games = self.scrape_games 
+		games.each do | game | 
+		 	scores << (game.score_w.to_i - game.score_l.to_i)
+		end 
+		puts "the biggest margin of victory today was #{scores.max} runs!".green 	
+	end
 
 
 	end 
